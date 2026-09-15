3869e8a6b7d9d4e70ec4042ca67d0d61e8876e5d
Claudio Linconao <cl.linconao@gmail.com>
2026-09-14 23:11:07 -0300

    Configurar base path y compilar para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Inaf-Lafken/', 
+    base: '/Inaf-Lafken/', // <--- Revisa que esto esté escrito exactamente así
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
 });
